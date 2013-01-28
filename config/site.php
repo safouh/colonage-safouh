7413968f464d3593c81b8667acad7b4238aa04f2
safouh <safouhmehdi@gmail.com>
2013-01-28 12:19:15 +0100

    concrete5 partie 1
    Signed-off-by: safouh <safouhmehdi@gmail.com>

---
+++ config/site.php
@@ -0,0 +1,6 @@
+<?php 
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'concrete');
+define('PASSWORD_SALT', 'FyZEg06bEXixOsuAVirz6JWKgCtoZFHZE4S1LRIRO9SAZQKzfqZE7TWIlFMTOO7m');
